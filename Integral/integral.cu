48f05a0831ce2483ae836b0b41d6e1bcd3461ce8
hugo <hugo.garciac@edu.uah.es>
2022-03-06 16:27:24 +0100

    Estructura basica

---
+++ Integral/integral.cu
@@ -0,0 +1,16 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+__global__ void kernelIntegral() {
+    
+}
+
+int main()
+{
+
+
+}
